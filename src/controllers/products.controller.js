eb43fba72ec4c6a303c2dbc5a1fa6b3d7c0522df
janet16ac <220256@utxicotepec.edu.mx>
2024-02-19 16:33:08 -0600

    Primeros cambios realizados 3

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,77 +2,76 @@ import productDao from "../dao/products.dao.js";
 const productController = {}
 
 productController.getAll = async (req, res) => {
-   productDao.getAll()
-   //PROMESA
-   .then(products => {
-    if(products != null){
-      res.render('../src/views/index.ejs',{
-         products
-      })
-    }else{
-      res.json({
-         "status":"not found"
-      })
-    }
-   }).catch(err => {
-    res.status(404).json("Error")
-   })
+    productDao.getAll()
+        .then(products => {
+            if (products != null) {
+                res.render('../src/views/index.ejs', {
+                    products
+                });
+            } else {
+                res.json({
+                    "status": "not found"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(404).json("Error");
+        });
 }
 
 productController.getOne = async (req, res) => {
-   productDao.getOne(req.params.barcode)
-   .then(product => {
-      if(product!= null){
-         res.render('../src/views/update.ejs',{
-            product
-         })
-   }
-   else{
-      res.json({
-         "status":"not found"
-      })
-   }
-   })
-   .catch(err => {
-      console.log(err.msg)
-      res.status(500).json({"status":"Server unaviable"})
-   })
+    productDao.getOne(req.params.barcode)
+        .then(product => {
+            if (product != null) {
+                res.render('../src/views/update.ejs', {
+                    product
+                });
+            } else {
+                res.json({
+                    "status": "not found"
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err.msg);
+            res.status(500).json({ "status": "Server unaviable" });
+        });
 }
 
 productController.insertOne = async (req, res) => {
-productDao.insertOne(req.body)
-.then(result => {
-  if(result)
-   res.redirect('/');
-}).catch(err => {
-   res.status(500).json({"status":"Server unaviable"})
-})
+    productDao.insertOne(req.body)
+        .then(result => {
+            if (result) {
+                res.redirect('/');
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ "status": "Server unaviable" });
+        });
 }
 
 productController.updateOne = async (req, res) => {
-   productDao.updateOne(req.params.barcode, req.body)
-   .then(result => {
-      if(result){
-         res.status(200).redirect('/')
-      }
-   })
-   .catch(err => {
-      res.status(500).json({"status":"Server unaviable"})
-   })
+    productDao.updateOne(req.params.barcode, req.body)
+        .then(result => {
+            if (result) {
+                res.status(200).redirect('/');
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ "status": "Server unaviable" });
+        });
 }
 
 productController.deleteOne = async (req, res) => {
-   productDao.deleteOne(req.params.barcode)
-   .then(result => {
-      if(result){
-         res.status(200).redirect('/')
-      }
-   })
-   .catch(err => {
-      res.status(500).json({"status":"Server unaviable"})
-   })
-
+    productDao.deleteOne(req.params.barcode)
+        .then(result => {
+            if (result) {
+                res.status(200).redirect('/');
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ "status": "Server unaviable" });
+        });
 }
-
 
 export default productController;
